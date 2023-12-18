a65da78e681f70f7c657ec45dd0632b962e5dbbd
SharrrkNado <tjark12@gmail.com>
2023-12-18 19:39:26 +0100

    initial

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,34 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild, signal } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+
+const areaPadding = 300;
 
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [CommonModule, RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
 export class AppComponent {
-  title = 'try-git-page';
+  score = signal(0);
+
+  @ViewChild('point') point!: ElementRef<HTMLDivElement>;
+  @ViewChild('arena') arena!: ElementRef<HTMLDivElement>;
+
+  pointClicked() {
+    this.score.update((score) => score + 1);
+
+    const width = this.arena.nativeElement.clientWidth;
+    const height = this.arena.nativeElement.clientWidth;
+
+    const newPos = {
+      x: Math.random() * (width - 2 * areaPadding) + +areaPadding + 'px',
+      y: Math.random() * (height - 2 * areaPadding) + +areaPadding + 'px',
+    };
+
+    this.point.nativeElement.style.left = newPos.x;
+    this.point.nativeElement.style.top = newPos.y;
+  }
 }
